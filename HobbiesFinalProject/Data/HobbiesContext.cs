1d6aef8e561d6d281c80e940fa4592718676c194
Jeremy Shepherd <JeremyLShepherd@gmail.com>
2021-04-16 12:52:01 -0400

    Created Data folder and HobbiesContext file.

---
+++ HobbiesFinalProject/Data/HobbiesContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HobbiesFinalProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HobbiesFinalProject.Data
+{
+    public class HobbiesContext : DbContext
+    {
+        public HobbiesContext(DbContextOptions<HobbiesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VideoGame> VideoGames { get; set; }
+        public DbSet<TeamMember> TeamMembers { get; set; }
+    }
+}
