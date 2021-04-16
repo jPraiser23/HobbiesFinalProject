a8c549287734dccdd592356e7bd11ca09c45068c
Jeremy Shepherd <JeremyLShepherd@gmail.com>
2021-04-16 11:33:29 -0400

    Created VideoGame model.

---
+++ HobbiesFinalProject/Models/VideoGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HobbiesFinalProject.Models
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string System { get; set; }
+    }
+}
